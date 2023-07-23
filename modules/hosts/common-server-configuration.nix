07cbde1f76b68724c9af107d43d3787fc596da09
mdlsvensson <wilmer.lindau@gmail.com>
2023-07-23 02:33:19 +0200

    common server configs, boilerplate

---
+++ modules/hosts/common-server-configuration.nix
@@ -0,0 +1,23 @@
+{
+  # https://nixos.wiki/wiki/Networking
+  networking.hostName = "server";
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+}
