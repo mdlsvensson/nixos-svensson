7bc782b9d6d217b57776f0c636b7c9cd18a95b4f
mdlsvensson <wilmer.lindau@gmail.com>
2023-07-23 02:31:57 +0200

    formatting

--- modules/hosts/common-configuration.nix
+++ modules/hosts/common-configuration.nix
@@ -6,19 +6,23 @@
   # === https://nixos.wiki/wiki/Xorg === https://nixos.wiki/wiki/XMonad === https://nixos.wiki/wiki/Xfce ===
   # ========================================================================================================
   services.xserver.enable = true;
-  services.xserver.layout = "us"; # Keyboard layout
-  services.xserver.libinput.mouse.accelProfile = "flat"; # Disable mouse acceleration.
-  services.xserver.desktopManager.xmonad.enable = true; # Main WM
-  services.xserver.desktopManager.xfce.enable = true; # Fallback DE and available for family members who want to check their email while over for dinner.
-  services.xserver.displayManager.lightdm.enable = true; # LightDM Display Manager (the login screen, aka "greeter").
+  services.xserver.layout = "us";                          # Keyboard layout.
+  services.xserver.libinput.mouse.accelProfile = "flat";   # Disable mouse acceleration.
+
+  # DESKTOP MANAGER
+  services.xserver.desktopManager.xmonad.enable = true;    # Main WM
+  services.xserver.desktopManager.xfce.enable = true;      # Fallback DE and available for family members who want to check their email while over for dinner.
+
+  # DISPLAY MANAGER
+  services.xserver.displayManager.lightdm.enable = true;   # LightDM Display Manager (the login screen, aka "greeter").
   services.xserver.displayManager.defaultSession = "xfce"; # Session can be set in the top-right while loggin in.
 
-  services.xserver.serverFlagsSection = '' # Disable all "monitor turn off", "screen-savery"-type behavior.
+  services.xserver.serverFlagsSection = ''
     Option "BlankTime" "0"
     Option "StandbyTime" "0"
     Option "SuspendTime" "0"
     Option "OffTime" "0"
-  '';
+  '';                                                      # Disable all "monitor turn off", "screen-savery"-type behavior.
 
   # =========================================================================
   # === COMPOSITOR === I use picom for the vSync, sorts my screen-tearing ===
