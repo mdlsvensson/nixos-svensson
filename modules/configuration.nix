593f26d3d4fb527ab2134bb3deff7bec6d278ef2
mdlsvensson <wilmer.lindau@gmail.com>
2023-07-23 02:13:41 +0200

    packages

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -57,8 +57,19 @@
   # ===============================================================================
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    wget
+    file
+    vim
+    neofetch
+    killall
+    unzip
+    zip
+    inxi
+    htop
+    fd
+    gnutar
+    zenith
+    git
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
