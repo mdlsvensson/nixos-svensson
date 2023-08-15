d65ac25b9fba6b759a1014d754055d92044efb11
mdlsvensson <wilmer.lindau@gmail.com>
2023-08-16 01:51:55 +0200

    comments

--- flake.nix
+++ flake.nix
@@ -2,23 +2,17 @@
   description = "NixOS flake dektop/laptop/servers, beginner-friendly commenting (https://github.com/mdlsvensson)";
 
   inputs = {
-    # Unstable channel for packages that need it.
-    # https://channels.nixos.org/
-    nixpkgs.url = "github:NixOS/nixpkgs/23.05";
-    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/23.05";                           # https://channels.nixos.org/
+    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";                 # Unstable channel for packages that need it.
 
-    # mdlsvensson/nixos-setup-config | NixOS module for defining local configuration data: https://github.com/mdlsvensson/nixos-setup-config
-    setup-config.url = "github:mdlsvensson/nixos-setup-config";
+    setup-config.url = "github:mdlsvensson/nixos-setup-config";           # https://github.com/mdlsvensson/nixos-setup-config
     setup-config.inputs.nixpkgs.follows = "nixpkgs";
-    # nix-community/nixvim | Configure Neovim with Nix: https://github.com/nix-community/nixvim
-    nixvim.url = "github:nix-community/nixvim";
+    nixvim.url = "github:nix-community/nixvim";                           # https://github.com/nix-community/nixvim
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
-    # Misterio77/nix-colors | Modules and schemes to make theming with Nix awesome: https://github.com/Misterio77/nix-colors
-    nix-colors.url = "github:misterio77/nix-colors";
+    nix-colors.url = "github:misterio77/nix-colors";                      # https://github.com/Misterio77/nix-colors
     nix-colors.inputs.nixpkgs.follows = "nixpkgs";
 
-    # nix-community/home-manager | Manage a user environment using Nix: https://github.com/nix-community/home-manager
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.url = "github:nix-community/home-manager/release-23.05"; # https://github.com/nix-community/home-manager
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
