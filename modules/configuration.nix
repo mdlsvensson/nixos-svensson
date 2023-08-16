cde6dd5a2db8aad0c65c2d5aa98fe5f6f2c56da0
mdlsvensson <wilmer.lindau@gmail.com>
2023-08-16 04:03:44 +0200

    comments/timezone/locale

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -4,10 +4,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # "video" group for xbacklight
   # https://nixos.org/manual/nixos/stable/index.html#sec-user-management
   users.users.${config.setupConfig.user.username} = {
-    home = config.setupConfig.user.homeDirectory;
     isNormalUser = true;
     extraGroups = [ "networkmanager" "wheel" "video" ];
   };
@@ -17,9 +15,8 @@
   # https://nixos.wiki/wiki/Storage_optimization
   nix.settings.auto-optimise-store = true;
 
-  # Common packages for server-desktop-laptop
-  # unfree packages = restrictively lisenced
   nixpkgs.config.allowUnfree = true;
+  # Common packages for all hosts
   environment.systemPackages = with pkgs; [
     wget
     file
@@ -39,10 +36,7 @@
   sudo.extraConfig = "Defaults env_reset,pwfeedback"; # Shows passwords characters as asterisks
 
   networking.hostName = config.setupConfig.host;
-  networking.networkmanager.enable = true; # Enable network manager for all hosts
-
-  time.timeZone = "Europe/Stockholm";
-  i18n.defaultLocale = "en_US.UTF-8";
+  networking.networkmanager.enable = true;
 
   system.stateVersion = "23.05"; # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
 }
