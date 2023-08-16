7cb7212b1f1fd1ed77dde80d1aa73c4d3e8890c4
mdlsvensson <wilmer.lindau@gmail.com>
2023-08-16 11:01:38 +0200

    comments

--- modules/hosts/common/common-configuration.nix
+++ modules/hosts/common/common-configuration.nix
@@ -1,25 +1,24 @@
 { pkgs, ... }:
 {
-  # The plan is to add a sway config to this flake eventually
   # https://nixos.wiki/wiki/Xorg | https://nixos.wiki/wiki/XMonad
   services.xserver.enable = true;
   services.xserver.layout = "us";                          # Keyboard layout
   services.xserver.libinput.mouse.accelProfile = "flat";   # Disable mouse acceleration
 
   # DESKTOP MANAGER
-  services.xserver.desktopManager.xmonad.enable = true;    # Main WM
-  services.xserver.desktopManager.xfce.enable = true;      # Fallback DE and available for family members who want to check their email while over for dinner
+  services.xserver.desktopManager.xmonad.enable = true;    # Main
+  services.xserver.desktopManager.xfce.enable = true;      # Fallback
 
   # DISPLAY MANAGER
-  services.xserver.displayManager.lightdm.enable = true;   # LightDM Display Manager (the login screen, aka "greeter")
-  services.xserver.displayManager.defaultSession = "xfce"; # Session can be set in the top-right at login
+  services.xserver.displayManager.lightdm.enable = true;   # The login screen aka "greeter"
+  services.xserver.displayManager.defaultSession = "xfce";
 
   services.xserver.serverFlagsSection = ''
     Option "BlankTime" "0"
     Option "StandbyTime" "0"
     Option "SuspendTime" "0"
     Option "OffTime" "0"
-  '';                                                      # Disable all "monitor turn off", "screen-savery"-type behavior
+  '';
 
   # https://nixos.wiki/wiki/Picom
   services.picom.enable = true;
@@ -30,11 +29,11 @@
   # https://nixos.wiki/wiki/PipeWire 
   sound.enable = true;                        # Setting to false can fix pipewire sound if you had this enabled before
   hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;               # Pipewire needs realtimeKit enabled
+  security.rtkit.enable = true;
   services.pipewire.enable = true;
   services.pipewire.alsa.enable = true;
   services.pipewire.alsa.support32Bit = true; # 32bit support if you run 64bit system
-  services.pipewire.pulse.enable = true;      # Pulse server emulation. Can use pulse tools and gui
+  services.pipewire.pulse.enable = true;      # Pulse server emulation
 
   # You can pick specific nerdfonts with this override
   # https://nixos.wiki/wiki/Fonts | https://github.com/ryanoasis/nerd-fonts
