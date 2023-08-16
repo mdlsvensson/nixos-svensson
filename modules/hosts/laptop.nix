68bb1155b43d1dee5be1d6982cb00388411a1e3c
mdlsvensson <wilmer.lindau@gmail.com>
2023-08-16 11:01:01 +0200

    comments

--- modules/hosts/laptop.nix
+++ modules/hosts/laptop.nix
@@ -1,9 +1,7 @@
-{ inputs, ... }:
+{ ... }:
 {
   imports = [ ./common/common-configuration.nix ];
 
-  # BACKLIGHT
   hardware.acpilight.enable = true;
-  # TOUCHPAD
   services.xserver.libinput.touchpad.accelProfile = "flat";
 }
