1c9ba325efbbf7094436b137b46759c27dad5ec4
mdlsvensson <wilmer.lindau@gmail.com>
2023-08-19 10:02:16 +0200

    applied setupConfig vars and fixed pkgs config

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -7,13 +7,31 @@
 
   colorScheme = inputs.nix-colors.colorSchemes.catppuccin-mocha;
 
-  home.username = ${user};
-  home.homeDirectory = ${homePath};
+  home.username = config.setupConfig.user.username;
+  home.homeDirectory = config.setupConfig.user.homeDirectory;
   # home.packages = with pkgs; [];
 
-  nixpkgs.allowUnfree = true;
-  # Workaround for https://github.com/nix-community/home-manager/issues/2942
-  nixpkgs.allowUnfree = allowUnfreePredicate = (_: true);
+  nixpkgs = {
+    # You can add overlays here
+    overlays = [
+      # If you want to use overlays exported from other flakes:
+      # neovim-nightly-overlay.overlays.default
+
+      # Or define it inline, for example:
+      # (final: prev: {
+      #   hi = final.hello.overrideAttrs (oldAttrs: {
+      #     patches = [ ./change-hello-to-hi.patch ];
+      #   });
+      # })
+    ];
+    # Configure your nixpkgs instance
+    config = {
+      # Disable if you don't want unfree packages
+      allowUnfree = true;
+      # Workaround for https://github.com/nix-community/home-manager/issues/2942
+      allowUnfreePredicate = (_: true);
+    };
+  };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
