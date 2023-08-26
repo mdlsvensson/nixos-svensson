0ba27e2035f5ca6d0dc024e53293050c776481df
mdlsvensson <wilmer.lindau@gmail.com>
2023-08-26 03:46:26 +0200

    default session

--- modules/hosts/common/common-configuration.nix
+++ modules/hosts/common/common-configuration.nix
@@ -7,7 +7,7 @@
   services.xserver.windowManager.i3.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
   services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.displayManager.defaultSession = "xfce";
+  services.xserver.displayManager.defaultSession = "none+i3";
 
   services.xserver.serverFlagsSection = ''
     Option "BlankTime" "0"
