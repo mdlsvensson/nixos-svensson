742fe470d66130df7320ae4e2758ffe2f6edbeb0
mdlsvensson <wilmer.lindau@gmail.com>
2023-08-27 14:29:36 +0200

    changed to unstable

--- flake.nix
+++ flake.nix
@@ -2,20 +2,19 @@
   description = "Personal NixOS configuration for dektop/laptop - nothing fancy.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/23.05";                            # https://channels.nixos.org/
-    unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";                            # https://channels.nixos.org/
     nixvim.url = "github:nix-community/nixvim";                            # https://github.com/nix-community/nixvim
-    nixvim.inputs.nixpkgs.follows = "unstable";
+    nixvim.inputs.nixpkgs.follows = "nixpkgs";
     nix-colors.url = "github:misterio77/nix-colors";                       # https://github.com/Misterio77/nix-colors
     nix-colors.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager/release-23.05";  # https://github.com/nix-community/home-manager
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, unstable, nixvim, nix-colors, home-manager, ... } @ inputs:
+  outputs = { nixpkgs, nixvim, nix-colors, home-manager, ... } @ inputs:
     let
       system = "x86_64-linux";  # System architecture
-      host = "laptop";          # The configuration to apply
+      host = "desktop";          # The configuration to apply
     in {
       nixosConfigurations = {
         svensson = nixpkgs.lib.nixosSystem {
