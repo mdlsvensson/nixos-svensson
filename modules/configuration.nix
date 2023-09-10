c2fdd73f1fc57f1c0acb091b43199f271d7ae4dc
mdlsvensson <wilmer.lindau@gmail.com>
2023-09-11 00:20:51 +0200

    set hostname to arg

--- modules/configuration.nix
+++ modules/configuration.nix
@@ -1,15 +1,10 @@
-{ config, pkgs, ... }:
+{ config, pkgs, host, ... }:
 {
-  # Disable systemd emergency shell
-  systemd.enableEmergencyMode = false;
-
   # https://nixos.wiki/wiki/Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Stockholm";
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   # https://nixos.org/manual/nixos/stable/index.html#sec-user-management
@@ -35,14 +30,14 @@
     inxi
     htop
     fd
-    gnutar
+    # gnutar
     zenith
     git
   ];
 
   security.sudo.extraConfig = "Defaults env_reset,pwfeedback"; # Shows passwords characters as asterisks
 
-  networking.hostName = "laptop";
+  networking.hostName = host;
   networking.networkmanager.enable = true;
 
   system.stateVersion = "23.05"; # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
