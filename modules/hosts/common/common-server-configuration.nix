456a8fa982535f54c84bf412918dd8c8f777de7e
mdlsvensson <wilmer.lindau@gmail.com>
2023-09-11 00:22:47 +0200

    host name set in config.nix instead

--- modules/hosts/common/common-server-configuration.nix
+++ modules/hosts/common/common-server-configuration.nix
@@ -1,7 +1,4 @@
 {
-  # https://nixos.wiki/wiki/Networking
-  networking.hostName = "server";
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
