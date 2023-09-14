5f08125a81b5e2d74435b2ac96e73df4a7e5016c
mdlsvensson <wilmer.lindau@gmail.com>
2023-09-14 03:27:00 +0200

    i3status fixed

--- modules/home-manager/programs/i3status-rust.nix
+++ modules/home-manager/programs/i3status-rust.nix
@@ -1,39 +1,48 @@
-{ ... }:
 {
   enable = true;
-  default = {
-    blocks = [
-      {
-        alert = 10.0;
-        block = "disk_space";
-        info_type = "available";
-        interval = 60;
-        path = "/";
-        warning = 20.0;
-      }
-      {
-        block = "memory";
-        format = " $icon mem_used_percents ";
-        format_alt = " $icon $swap_used_percents ";
-      }
-      {
-        block = "cpu";
-        interval = 1;
-      }
-      {
-        block = "load";
-        format = " $icon $1m ";
-        interval = 1;
-      }
-      {
-        block = "sound";
-      }
-      {
-        block = "time";
-        format = " $timestamp.datetime(f:'%a %d/%m %R') ";
-        interval = 60;
-      }
-    ];
+  bars = {
+    top = {
+      blocks = [
+        {
+          block = "disk_space";
+          path = "/";
+          info_type = "available";
+          interval = 60;
+          warning = 20.0;
+          alert = 10.0;
+        }
+        {
+          block = "memory";
+          format_mem = " $icon $mem_used_percents ";
+          format_swap = " $icon $swap_used_percents ";
+        }
+        {
+          block = "cpu";
+          interval = 1;
+        }
+        {
+          block = "load";
+          interval = 1;
+          format = " $icon $1m ";
+        }
+        { block = "sound"; }
+        {
+          block = "time";
+          interval = 60;
+          format = " $timestamp.datetime(f:'%a %d/%m %R') ";
+        }
+      ];
+      settings = {
+        theme =  {
+          theme = "solarized-dark";
+          overrides = {
+            idle_bg = "#123456";
+            idle_fg = "#abcdef";
+          };
+        };
+      };
+      icons = "awesome5";
+      theme = "gruvbox-dark";
+    };
   };
 }
-
