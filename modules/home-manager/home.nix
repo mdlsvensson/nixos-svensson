9c0d04e6acd3a95de1489e98bfa4f2789cc7f4c2
mdlsvensson <wilmer.lindau@gmail.com>
2023-09-27 16:05:17 +0200

    fixing invalid rofi syntax

--- modules/home-manager/home.nix
+++ modules/home-manager/home.nix
@@ -81,11 +81,7 @@
     kitty = import ./programs/kitty.nix { inherit (config.colorScheme) colors; };
     nixvim = import ./programs/nixvim.nix;
     i3status-rust = import ./programs/i3status-rust.nix;
-    rofi = {
-      enable = true;
-      location = "top";
-      theme = import ./programs/rofi-theme.nix;
-    };
+    rofi = import ./programs/rofi;
     git = {
       enable = true;
       userName  = "mdlsvensson";
---
+++ modules/home-manager/programs/rofi/default.nix
@@ -0,0 +1,5 @@
+{
+  enable = true;
+  location = "top";
+  theme = ./themes/darkness.rasi;
+}
