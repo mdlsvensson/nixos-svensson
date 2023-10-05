8a8e2723d23e412d9e1411b1bf4e383a48e8f34f
mdlsvensson <wilmer.lindau@gmail.com>
2023-10-05 16:38:27 +0200

    formatting

--- modules/hosts/common/common-configuration.nix
+++ modules/hosts/common/common-configuration.nix
@@ -1,11 +1,10 @@
 { pkgs, ... }:
-  let
-    onLogin = ''
-      xrandr --output DisplayPort-0 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x288 --rotate normal
-      feh --bg-tile ~/Pictures/wallpaper.png
-    '';
-  in
-{
+let
+  onLogin = ''
+    xrandr --output DisplayPort-0 --mode 1920x1080 --pos 1920x0 --rotate left --output HDMI-A-0 --primary --mode 1920x1080 --pos 0x288 --rotate normal
+    feh --bg-tile ~/Pictures/wallpaper.png
+  '';
+in {
   # https://nixos.wiki/wiki/Xorg | https://nixos.wiki/wiki/XMonad
   services.xserver = {
     enable = true;
