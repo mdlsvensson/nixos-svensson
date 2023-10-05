f8f241e14055eac84d89f2ce36ea5396180ceb36
mdlsvensson <wilmer.lindau@gmail.com>
2023-10-05 15:00:34 +0200

    noto nerd font

--- modules/hosts/common/common-configuration.nix
+++ modules/hosts/common/common-configuration.nix
@@ -67,7 +67,7 @@
 
   # https://nixos.wiki/wiki/Fonts | https://github.com/ryanoasis/nerd-fonts
   fonts.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "Iosevka" "JetBrainsMono" ]; })
+    (nerdfonts.override { fonts = [ "Iosevka" "JetBrainsMono" "Noto" ]; })
   ];
 
   environment.systemPackages = with pkgs; [
