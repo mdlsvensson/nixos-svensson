b3d117f2b2edbec7b6593c9d6c567efd54ed0b1e
mdlsvensson <wilmer.lindau@gmail.com>
2024-02-12 17:36:18 +0100

    vscode extensions

--- modules/dev-flakes/vscodium/flake.nix
+++ modules/dev-flakes/vscodium/flake.nix
@@ -3,6 +3,8 @@
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     flake-utils.follows = "nix-vscode-extensions/flake-utils";
     nixpkgs.follows = "nix-vscode-extensions/nixpkgs";
+
+    common-extensions.url = "/home/mdlsvensson/Repo/nixos-svensson/modules/dev-flakes/common-vscode-extensions";
   };
 
   outputs = inputs:
@@ -16,10 +18,10 @@
           packages.default =
             vscode-with-extensions.override {
               vscode = vscodium;
-              vscodeExtensions = [
-                extensions.vscode-marketplace.bedirt.gpt-token-counter-live
-                extensions.vscode-marketplace.spaceshaman.spaceshaman-dark
-                extensions.open-vsx.geequlim.godot-tools
+              vscodeExtensions = with extensions; [
+                open-vsx.geequlim.godot-tools
+                vscode-marketplace.bedirt.gpt-token-counter-live
+                vscode-marketplace.spaceshaman.spaceshaman-dark
               ];
             };
 
