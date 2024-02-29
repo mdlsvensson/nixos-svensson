d245dc5ac1aacecc6dbeb8422e7ecce84b361897
mdlsvensson <wilmer.lindau@gmail.com>
2024-02-29 14:57:50 +0100

    refactor, add wikipedia search, add bookmarks

--- modules/home-manager/programs/firefox.nix
+++ modules/home-manager/programs/firefox.nix
@@ -1,8 +1,8 @@
 {
   programs.firefox.enable = true;
-  programs.firefox.profiles."mdlsvensson".search = {
-    force = true;
-    engines = {
+  programs.firefox.profiles."mdlsvensson" = {
+    search.force = true;
+    search.engines = let everyDay = 24 * 60 * 60 * 1000; in {
       "nixpkgs" = {
         urls = [{
           template = "https://search.nixos.org/packages";
@@ -12,10 +12,10 @@
           ];
         }];
         iconUpdateURL = "https://nixos.wiki/favicon.png";
-        updateInterval = 24 * 60 * 60 * 1000; # every day
+        updateInterval = everyDay;
         definedAliases = [ "@pkgs" ];
       };
-      "opts" = {
+      "nixos-options" = {
         urls = [{
           template = "https://search.nixos.org/options";
           params = [
@@ -24,7 +24,7 @@
           ];
         }];
         iconUpdateURL = "https://nixos.wiki/favicon.png";
-        updateInterval = 24 * 60 * 60 * 1000; # every day
+        updateInterval = everyDay;
         definedAliases = [ "@opts" ];
       };
       "home-manager" = {
@@ -35,7 +35,7 @@
           ];
         }];
         iconUpdateURL = "https://mipmip.github.io/home-manager-option-search/images/favicon.png";
-        updateInterval = 24 * 60 * 60 * 1000; # every day
+        updateInterval = everyDay;
         definedAliases = [ "@hm" ];
       };
       "youtube" = {
@@ -46,9 +46,33 @@
           ];
         }];
         iconUpdateURL = "https://s.ytimg.com/yts/img/favicon-vfl8qSV2F.ico";
-        updateInterval = 24 * 60 * 60 * 1000; # every day
+        updateInterval = everyDay;
         definedAliases = [ "@yt" ];
       };
+      "wikipedia" = {
+        urls = [{
+          template = "https://en.wikipedia.org/w/index.php";
+          params = [
+            { name = "search"; value = "{searchTerms}"; }
+          ];
+        }];
+        iconUpdateURL = "https://en.wikipedia.org/favicon.ico";
+        updateInterval = everyDay;
+        definedAliases = [ "@wiki" ];
+      };
     };
+    bookmarks = [
+      {
+        name = "google-gemini";
+        tags = [ "ai" "bot" "google" "chat" ];
+        keyword = "ge";
+        url = "https://gemini.google.com/";
+      }
+      {
+        name = "easing-functions-cheat-sheet";
+        tags = [ "math" "programming" "logic" "gamedev" "trigonometry" ];
+        url = "https://easings.net/";
+      }
+    ];
   };
 }
